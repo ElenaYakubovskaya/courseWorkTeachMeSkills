538a5887c43941fcfee59f6966c86adceeff43c4
Elena Yakubovskaya <70071504+jalenka92@users.noreply.github.com>
2021-11-18 11:46:25 +0300

    code refactoring

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
-const taskInput = document.querySelector('list-item-task-input');
-const addTaskBtn = document.querySelector('list-item-task-btn');
+const addTaskBtn = document.getElementById('toDoBtn');
+const taskInput = document.getElementById('inputToDo');
+const alllists = document.querySelector('.list-item');
+
+let tasks = [];
+
+function TaskToDo(description) {
+  this.description = description;
+  this.completed = false;
+}
+console.log(addTaskBtn);
+addTaskBtn.addEventListener('click', () => {
+  tasks.push(new TaskToDo(taskInput.value));
+  console.log(tasks);
+});
